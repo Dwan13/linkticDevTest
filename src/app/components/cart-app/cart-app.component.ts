90f28573f59414d80719b4bca7c9d772face8cea
Dwan Veloza <dwanveloza@gmail.com>
2025-05-14 13:48:44 -0500

    feat(layout): setup base layout structure (Task:1, Prio:High)

---
+++ src/app/components/cart-app/cart-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-app',
+  imports: [],
+  templateUrl: './cart-app.component.html',
+  styleUrl: './cart-app.component.sass'
+})
+export class CartAppComponent {
+
+}
